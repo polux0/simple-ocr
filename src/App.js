8b7be4c856ed1ad92b86a2bf210766f3df7d5213
polux0 <afidelo@protonmail.com>
2023-12-07 14:25:55 +0100

    fix: enviornment camera & logs below camera frame;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
 import Webcam from 'react-webcam';
 import Tesseract from 'tesseract.js';
 
 const App = () => {
   const webcamRef = useRef(null);
+  const [ocrText, setOcrText] = useState(''); // State to store OCR results
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -16,9 +17,16 @@ const App = () => {
         }
       ).then(({ data: { text } }) => {
         console.log(text);
+        setOcrText(text); // Update state with OCR results
       });
     }
   }, [webcamRef]);
+
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: 'environment',
+  };
 
   return (
     <div>
@@ -28,8 +36,13 @@ const App = () => {
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         width={1280}
+        videoConstraints={videoConstraints}
       />
       <button onClick={capture}>Capture</button>
+      <div>
+        <h2>OCR Results:</h2>
+        <p>{ocrText}</p> {/* Display OCR results here */}
+      </div>
     </div>
   );
 };
